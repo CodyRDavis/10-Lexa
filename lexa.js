90c927a0c0853cff149d27d7535ac64430f38a44
Cody Davis <codydavis@codys-imac.lan>
2018-11-20 18:33:49 -0600

    spotify query, movies, bands done. added catches for errors.

--- lexa.js
+++ lexa.js
@@ -16,19 +16,47 @@ const command = nodeArgs[2];
 const searchTerm = (nodeArgs.slice(3)).join("+");
 let queryUrl = "";
 
-//formatting searchTerm into something usable for API query
-
-console.log (command,searchTerm);
-
+//switch is set for the command the user gave, looking for the correct keywords
 switch (command){
     case "spotify-this":
-        console.log("spotify");
-        queryUrl = "gi"
+
+        //checking to see if a song was provide, if not defaults to the sign
+        let spotifySearch = "";
+        if(searchTerm===""){
+            spotifySearch="the+sign"
+        }else{
+            spotifySearch=searchTerm;
+        }
+
+        //call out to spotify for 5 results.
+        spotify.search({ 
+            type: 'track', query: spotifySearch, limit: 5
+        }).then(function(response) {
+            if(response.items.length === 0){
+                console.log("No songs by that title found.")
+            }
+            //each song sent back, we print out the info for it.
+            response.tracks.items.forEach(function(result){
+                console.log();
+                console.log ("Artist(s):  "+result.artists[0].name);
+                console.log ("Song Title: "+result.name);
+                console.log ("Album:      "+result.album.name);
+                console.log ("On Spotify: "+result.external_urls.spotify);
+            })
+            
+        })
+        //incase something goes wrong
+        .catch(function(err) {
+            console.log("No song by that name found");
+        });
         break;
+
     case "concert-this":
+        //using axios to call out to bandsintown and get back results
         queryUrl = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp";
         axios.get(queryUrl).then(function(response){
-            //TODO format the TIME#####################################################################################################################
+
+            //for each gig a band has, print out the info
             response.data.forEach(function(gig){
                 let date = gig.datetime.split("T");
                 let formatedDate = moment(date[0]).format("MM/DD/YYYY");
@@ -39,8 +67,13 @@ Location:   ${venue.city}, ${venue.region}, ${venue.country}
 Date of show: ${formatedDate} 
                 `)
             });
+        })
+        //if something goes wrong...
+        .catch(function(err){
+            console.log ("Band not found");
         });
         break;
+
     case "movie-this":
         queryUrl = "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy";
         axios.get(queryUrl).then(function(response) {
@@ -55,6 +88,10 @@ Language:     ${movie.Language}
 Movie Plot:   ${movie.Plot}
 Actors:       ${movie.Actors}
             `)
+        })
+        //incase something goes wrong.
+        .catch(function(err){
+            console.log("No movies by that name found");
         });
         break;
     default:
